d2a1db2e6edcda444d4a3da7f947fae32a268ff9
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-03-07 12:25:43 +0530

    Time: 265 ms (7.64%) | Memory: 42.7 MB (16.47%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -4,19 +4,19 @@ public:
         if(grid[0][0]!=0 || grid[grid.size()-1][grid[0].size()-1]!=0) return -1;
        int m = grid.size();
        int n = grid[0].size();
-       set<vector<int>>st;
+       queue<vector<int>>st;
        vector<vector<int>>dist(m,vector<int>(n,1e9));
-       st.insert({1,0,0});
+       st.push({1,0,0});
        vector<int>drow = {0,0,1,-1,1,-1,1,-1};
        vector<int>dcol = {1,-1,0,0,1,-1,-1,1};
        dist[0][0]=1;
 
        while(!st.empty()){
-           auto vec1 = *st.begin();
+           auto vec1 = st.front();
            int wt=vec1[0];
            int x = vec1[1];
            int y = vec1[2];
-           st.erase(vec1);
+           st.pop();
            for(int i=0;i<8;i++){
                int row = x+drow[i];
                int col = y+dcol[i];
@@ -24,12 +24,10 @@ public:
                    
                    if(wt+1<dist[row][col]){
                        cout<<row<<" "<<col<<" ";
-                       if(dist[row][col]!=1e9){
-                           st.erase({dist[row][col],row,col});
-                       }
+                       
                        dist[row][col]=wt+1;
                        cout<<dist[row][col]<<endl;
-                       st.insert({dist[row][col],row,col});
+                       st.push({dist[row][col],row,col});
                    }
                }
            }
