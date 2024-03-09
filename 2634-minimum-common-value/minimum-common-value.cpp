8a40a540286fbc32c80f32866dde49eb46fecb15
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-03-09 12:25:55 +0530

    Time: 27 ms (99.67%) | Memory: 52.6 MB (86.28%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int l =0,r=0;
         while(l<nums1.size() && r<nums2.size()){
             if(nums1[l]==nums2[r]) return nums1[l];
