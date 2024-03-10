fbd01794cc9f9bf2595eacfde8fa9ffe5415c09b
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-03-10 13:06:29 +0530

    Time: 63 ms (38.68%) | Memory: 39.5 MB (5.94%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,37 @@
+#define ll long long
+#define pll pair<long long,long long>
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        unordered_map<ll,vector<pair<ll,ll>>>adj;
+        for(auto it:roads){
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
+
+        }
+        int modd = 1e9+7;
+        vector<ll> dist(n, LONG_MAX);
+        vector<ll> ways(n);
+        ways[0] = 1;
+        dist[0] = 0;
+        priority_queue<pll, vector<pll>, greater<>> minHeap;
+        minHeap.push({0, 0}); 
+        while (!minHeap.empty()) {
+            auto[d, u] = minHeap.top(); minHeap.pop();
+            if (d > dist[u]) continue; 
+            for(auto [v, time] : adj[u]) {
+                if (dist[v] > d + time) {
+                    dist[v] = d + time;
+                    ways[v] = ways[u];
+                    minHeap.push({dist[v], v});
+                } else if (dist[v] == d + time) {
+                    ways[v] = (ways[v] + ways[u]) % modd;
+                }
+            }
+        }
+        return ways[n-1];
+    }
+};
