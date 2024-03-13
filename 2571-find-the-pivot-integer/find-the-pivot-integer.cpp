47f5d5e1a4b35c164efb56061c7a1519a5ba3747
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-03-13 09:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -2,12 +2,23 @@ class Solution {
 public:
     int pivotInteger(int n) {
         int sum1 = n*(n+1)/2;
-        int sum2=n;
-        while(sum1>sum2 && n>0){
-            sum1-=n;
-            sum2+=n-1;
-            n--;
+        // int sum2=n;
+        // while(sum1>sum2 && n>0){
+        //     sum1-=n;
+        //     sum2+=n-1;
+        //     n--;
+        // }
+        // return sum1==sum2?n:-1;
+        int l = 1,h= n+1;
+        while(l<h){
+            int mid = l+(h-l)/2;
+            int lsum = mid*(mid-1)/2;
+            int hsum = sum1 - (mid+1)*mid/2;
+            if(lsum<hsum){
+                l = mid+1;
+            }
+            else h=mid;
         }
-        return sum1==sum2?n:-1;
+        return (l-1)*l/2==sum1-(l*(l+1)/2)?l:-1;
     }
 };
