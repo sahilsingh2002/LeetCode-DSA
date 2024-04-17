ab5d1439a64b7352ffa27b4bd5b411d83450265b
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-04-17 09:46:52 +0530

    Time: 10 ms (48.64%) | Memory: 21.1 MB (21.32%) - LeetSync

---
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void inorder(TreeNode*root,string A,vector<string>&ans){
+        if(!root) return;
+        if(!root->left && !root->right){
+             char c = 'a'+root->val;
+               A=c+A;
+           if(A.size()>0) ans.push_back(A);
+           return;
+        }
+        char c = 'a'+root->val;
+        A=c+A;
+        inorder(root->left,A,ans);
+        inorder(root->right,A,ans);
+    }
+public:
+    string smallestFromLeaf(TreeNode* root) {
+        vector<string>ans;
+        inorder(root,"",ans);
+        sort(ans.begin(),ans.end());
+        return ans[0];
+    }
+};
