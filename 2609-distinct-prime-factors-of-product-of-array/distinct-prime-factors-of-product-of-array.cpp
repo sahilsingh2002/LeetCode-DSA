eb023d75f9b183ead7d90d29f93a188f39a32307
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-05-07 21:10:50 +0530

    Time: 30 ms (68.94%) | Memory: 21.1 MB (95.19%) - LeetSync

---
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    bool isPrime(int n){
+          if(n == 1 || n == 0)return false;
+   
+ 
+  for(int i = 2; i * i <= n; i++)
+  {
+    if(n % i == 0)return false;
+  }
+  return true;
+}
+public:
+    int distinctPrimeFactors(vector<int>& nums) {
+        int maxm = INT_MIN;
+        for(int i:nums){
+            maxm = max(maxm,i);
+        }
+        int cnt = 0;
+        vector<int>allPrimes;
+        for(int i=2;i<=maxm;i++){
+            if(isPrime(i)){
+                allPrimes.push_back(i);
+            }
+        }
+        for(int i:allPrimes){
+            bool divisible = false;
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]%i==0){
+                    nums[j]/=i;
+                    divisible = true;
+                    break;
+                }
+            }
+            cnt+=divisible;
+        }
+        return cnt;
+
+    }
+};
