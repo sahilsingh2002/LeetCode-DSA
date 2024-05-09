1abdf03a7e5fe09cc8ea556d81df0ee60baba5fc
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-05-09 13:34:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (31.81%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    int help(int n,vector<int>&dp){
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=help(n-1,dp)+help(n-2,dp);
+
+    }
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return help(n,dp);
+    }
+};
