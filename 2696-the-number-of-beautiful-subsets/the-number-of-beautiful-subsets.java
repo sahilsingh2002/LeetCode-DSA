5cfa76f6d63fbd3a224f56ee425830778e85305d
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-05-23 21:47:19 +0530

    Time: 898 ms (10.37%) | Memory: 44.7 MB (50.98%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+class Solution {
+    private int solve(int[] nums,int k,ArrayList<Integer> list,int i){
+        if(i==nums.length) return 0;
+        int p = nums[i];
+        int a = 0;
+        list.add(p);
+        if(!list.contains(p+k) && !list.contains(p-k)){
+            a = 1+solve(nums,k,list,i+1);
+        }
+        list.remove(list.size()-1);
+        int b = solve(nums,k,list,i+1);
+        return a+b;
+    }
+   
+    public int beautifulSubsets(int[] nums, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        return solve(nums,k,list,0);
+    }
+}
