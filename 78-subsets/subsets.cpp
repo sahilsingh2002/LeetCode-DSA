a38815718de1d4f5924b9155b2f6a1cf444d0a3f
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-05-23 09:44:11 +0530

    Time: 4 ms (38.66%) | Memory: 8.2 MB (94.16%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-void subs(vector<int>&nums,vector<vector<int>>&ans,int i,vector<int>&sub){
-    if(i>=nums.size()) {
-        ans.push_back(sub);
-        return;
-    };
-    sub.push_back(nums[i]);
-    subs(nums,ans,i+1,sub);
-    sub.pop_back();
-    subs(nums,ans,i+1,sub);
+// void subs(vector<int>&nums,vector<vector<int>>&ans,int i,vector<int>&sub){
+//     if(i>=nums.size()) {
+//         ans.push_back(sub);
+//         return;
+//     };
+//     sub.push_back(nums[i]);
+//     subs(nums,ans,i+1,sub);
+//     sub.pop_back();
+//     subs(nums,ans,i+1,sub);
 
-}
+// }
     vector<vector<int>> subsets(vector<int>& nums) {
-        // int n = nums.size();
+        
         // vector<vector<int>>ans;
-        // for(int i=0;i<(1<<n);i++){
-        //     vector<int>sub;
-        //     for(int j=0;j<n;j++){
-        //         if(i&(1<<j)){
-        //             sub.push_back(nums[j]);
-        //         }
-        //     }
-        //     ans.push_back(sub);
-        // }
-        // return ans;
-        vector<vector<int>>ans;
-        vector<int>sub;
-         subs(nums,ans,0,sub);
-         return ans;
+        // vector<int>sub;
+        //  subs(nums,ans,0,sub);
+        //  return ans;
+        vector<vector<int>>ans={{}};
+        for(int i:nums){
+            int size = ans.size();
+            for(int j=0;j<size;j++){
+                vector<int>subset = ans[j];
+                subset.push_back(i);
+                ans.push_back(subset);
+            }
+        }
+        return ans;
     }
 };
