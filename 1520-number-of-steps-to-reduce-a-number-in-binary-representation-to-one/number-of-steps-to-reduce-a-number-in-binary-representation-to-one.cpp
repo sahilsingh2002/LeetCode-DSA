468b5df763668204cb5e112f19b4d9fb7498c475
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-05-29 13:26:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (47.06%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,54 +1,14 @@
 class Solution {
 public:
     int numSteps(string s) {
-        vector<int>ones;
-        vector<int>zeros;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='0'){
-                zeros.push_back(i);
-
-            }
-            else{
-                ones.push_back(i);
+        int size = s.size();
+        int makezero = size-1,car = 0;
+        for(int i=size-1;i>0;i--){
+            if(s[i]-'0'+car==1){
+                makezero++;
+                car=1;
             }
         }
-        int r = s.size()-1;
-        int cnt = 0;
-        while(r>=0){
-
-            if(s[r]=='1' && s.size()>1){
-                
-                
-                s[r]='0';
-                if(zeros.size()>0){
-                int p = zeros[zeros.size()-1];
-                zeros.pop_back();
-                // cout<<p<<endl;
-                while(r>p){
-                    s[r]='0';
-                    r--;
-                }
-                s[r]='1';
-                cnt++;
-                cnt+=s.size()-(r+1);
-                s=s.substr(0,r+1);
-                // cout<<s<<endl;
-                }
-                else{
-                    cnt+=s.size()+1;
-                    return cnt;
-                }
-            }
-            else if(s[r]=='1' && s.size()==1) return cnt;
-            else{
-                s =s.substr(0,r);
-                // cout<<s<<endl;
-                zeros.pop_back();
-                r--;
-                cnt++;
-            }
-            // cout<<"r"<<r<<endl;
-        }
-        return cnt;
+        return makezero+car;
     }
 };
