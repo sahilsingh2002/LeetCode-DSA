85bb88a8fcd092c6e5240bf912d7a70fc6566eb9
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-06-17 19:26:59 +0530

    Time: 141 ms (5.00%) | Memory: 75.9 MB (18.45%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,36 +1,23 @@
 class Solution {
-    // vector<int>sqrs;
+    vector<int>sqrs;
 
 public:
     bool judgeSquareSum(int c) {
-    //     int x = sqrt(INT_MAX);
+        int x = sqrt(INT_MAX);
         
-    // for(int i=0;i<=x;i++){
-    //     sqrs.push_back(i*i);
-    // }    
-    // int sizer = min(c,(int)sqrs.size());
-    // if(c==0) return true;
-    // for(int i=0;i<sizer;i++){
+    for(int i=0;i<=x;i++){
+        sqrs.push_back(i*i);
+    }    
+    int sizer = min(c,(int)sqrs.size());
+    if(c==0) return true;
+    for(int i=0;i<sizer;i++){
 
-    //      int p = c-(sqrs[i]);
-    //     int it = *lower_bound(sqrs.begin(),sqrs.end(),p);
+         int p = c-(sqrs[i]);
+        int it = *lower_bound(sqrs.begin(),sqrs.end(),p);
         
-    //     if(it==p) return true;
-    // }
-    // return false;
-    // }
-    for (int divisor = 2; divisor * divisor <= c; divisor++) {
-            if (c % divisor == 0) {
-                int exponentCount = 0;
-                while (c % divisor == 0) {
-                    exponentCount++;
-                    c /= divisor;
-                }
-                if (divisor % 4 == 3 && exponentCount % 2 != 0) {
-                    return false;
-                }
-            }
-        }
-        return c % 4 != 3;
+        if(it==p) return true;
     }
+    return false;
+    }
+    
 };
