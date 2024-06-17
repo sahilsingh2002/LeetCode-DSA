211ac982898cfe19539419f3839468df56a0b6db
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-06-17 19:22:21 +0530

    Time: 248 ms (5.00%) | Memory: 76 MB (18.45%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    vector<int>sqrs;
+
+public:
+    bool judgeSquareSum(int c) {
+        int x = sqrt(INT_MAX);
+        
+    for(int i=0;i<=x;i++){
+        sqrs.push_back(i*i);
+    }    
+    int sizer = min(c,(int)sqrs.size());
+    if(c==0) return true;
+    for(int i=0;i<sizer;i++){
+
+         int p = c-(sqrs[i]);
+        int it = *lower_bound(sqrs.begin(),sqrs.end(),p);
+        
+        if(it==p) return true;
+    }
+    return false;
+    }
+};
