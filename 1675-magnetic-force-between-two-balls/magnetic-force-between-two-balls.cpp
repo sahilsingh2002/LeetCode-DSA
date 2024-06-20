42c221073699a94c09256e9555ee6c0734e1d2f7
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-06-20 11:44:42 +0530

    Time: 118 ms (84.80%) | Memory: 61.2 MB (95.31%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    bool canplace(vector<int>&position,int m,int mid){
+        int prev = position[0];
+        int cnt = 1;
+        for(int i=1;i<position.size();i++){
+            if(position[i]-prev>=mid){
+                prev = position[i];
+                cnt++;
+            }
+        }
+        return cnt>=m;
+    }
+public:
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(),position.end());
+        int n = position.size();
+       
+           int l = 1;
+           int r = position[n-1];
+            int ans = 1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(canplace(position,m,mid)){
+                ans = mid;
+                l=mid+1;
+            }
+            else r=mid-1;
+        }
+        return ans;
+    }
+};
