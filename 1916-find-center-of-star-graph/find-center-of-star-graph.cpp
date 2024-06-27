19e19090f60cf18d2225d70a9f95d00f385e39a5
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-06-27 08:38:15 +0530

    Time: 132 ms (72.15%) | Memory: 70.7 MB (92.91%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_set<int>nodes;
-        unordered_map<int,vector<int>>edg;
-        for(auto it:edges){
-            edg[it[0]].push_back(it[1]);
-            edg[it[1]].push_back(it[0]);
-            nodes.insert(it[0]);
-            nodes.insert(it[1]);
-        }
-        int n = nodes.size();
-        for(auto it:edg){
-            if(it.second.size()==n-1) return it.first;
-        }
-        return -1;
+
+        // Approach 2 specific if all given are stars
+        int i=edges[0][0];
+        int j = edges[0][1];
+        return (edges[1][0]==i || edges[1][1]==i?i:j);
+
+
     }
 };
+        // approach 1 - general approach
+        // unordered_set<int>nodes;
+        // unordered_map<int,vector<int>>edg;
+        // for(auto it:edges){
+        //     edg[it[0]].push_back(it[1]);
+        //     edg[it[1]].push_back(it[0]);
+        //     nodes.insert(it[0]);
+        //     nodes.insert(it[1]);
+        // }
+        // int n = nodes.size();
+        // for(auto it:edg){
+        //     if(it.second.size()==n-1) return it.first;
+        // }
+        // return -1;
