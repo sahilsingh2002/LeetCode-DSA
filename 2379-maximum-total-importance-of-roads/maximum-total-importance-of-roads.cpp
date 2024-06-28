9b520c5427fe6f9b72caece2054d045b821a6060
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-06-28 12:50:38 +0530

    Time: 300 ms (58.45%) | Memory: 133.9 MB (48.62%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -3,15 +3,27 @@ class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
         vector<int>conn(n,0);
+        vector<int>freq(n+1,0);
         for(auto it:roads){
             conn[it[0]]++;
             conn[it[1]]++;
 
         }
-        sort(conn.begin(),conn.end());
+        for(int i:conn){
+            freq[i]++;
+        }
+
+       
         long long sum = 0;
-        for(int i=0;i<n;i++){
-            sum+=(long long)conn[i]*(i+1);
+        long long curr = 1;
+        int idx = 0;
+        while(idx<=n){
+            while(freq[idx]>0){
+                sum+=(curr*idx);
+                curr++;
+                freq[idx]--;
+            }
+            idx++;
         }
             
         return sum;
