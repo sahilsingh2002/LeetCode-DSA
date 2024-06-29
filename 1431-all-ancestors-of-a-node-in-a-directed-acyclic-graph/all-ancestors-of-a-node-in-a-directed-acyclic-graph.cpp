ab03027dd92e48c65a0f63416047e5d48097c36d
sahil singh <74174130+sahilsingh2002@users.noreply.github.com>
2024-06-29 09:51:39 +0530

    Time: 82 ms (98.24%) | Memory: 63.7 MB (89.79%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,28 +1,25 @@
 class Solution {
-    void filldata(set<int>&par,vector<int>&vis,vector<int>adj[],int i){
-        if(vis[i] || adj[i].size()==0) return;
-            vis[i]=1;
-        for(auto it:adj[i]){
-            par.insert(it);
-            filldata(par,vis,adj,it);
+    void dfs(int curr,int par,vector<vector<int>>&ans,vector<int>adj[]){
+       for(int it:adj[curr]){
+        if(ans[it].empty() || ans[it].back()!=par){
+            ans[it].push_back(par);
+            dfs(it,par,ans,adj);
         }
+       }
     }
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         
         vector<int>adj[n];
         for(auto it:edges){
-            adj[it[1]].push_back(it[0]);
+            adj[it[0]].push_back(it[1]);
         }
         vector<vector<int>>ans(n);
-        for(int i=0;i<n;i++){
-            vector<int>vis(n,0);
-            set<int>par;
-            filldata(par,vis,adj,i);
-            vector<int>num(par.begin(),par.end());
-            ans[i]=num;
-           
-        }
+       
+            for(int i=0;i<n;i++){
+                dfs(i,i,ans,adj);
+            }
+        
         return ans;
     }
 };
